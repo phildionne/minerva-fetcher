f96770b57a6868a0c72d70925b04598b722fd401
Philippe Dionne <dionne.phil@gmail.com>
2014-03-29 15:39:53 -0400

    Configure specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'dotenv'
+Dotenv.load
+
+require 'bundler/setup'
+require 'rspec'
+
+require 'minerva_fetcher'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].map(&method(:require))
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.filter_run_excluding skip: true
+  config.run_all_when_everything_filtered = true
+end
