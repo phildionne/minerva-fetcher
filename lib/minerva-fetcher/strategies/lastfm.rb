0c93c5e858578f37b0883e1cf30e3d69b7440d53
Philippe Dionne <dionne.phil@gmail.com>
2014-03-29 17:26:25 -0400

    Create Lastfm strategy

---
+++ lib/minerva-fetcher/strategies/lastfm.rb
@@ -0,0 +1,112 @@
+require 'minerva-fetcher/strategy'
+
+module Minerva
+  module Fetcher
+    module Strategies
+
+      class Lastfm < Minerva::Fetcher::Strategy
+        class Response < Minerva::Fetcher::Response
+
+          # @return [Array]
+          def items
+            case
+            # track.getInfo
+            when response_raw.body.try(:[], 'track')
+              Array.wrap(response_raw.body['track'])
+
+            # track.search
+            when response_raw.body.try(:[], 'results').try(:[], 'trackmatches')
+              Array.wrap(response_raw.body['results']['trackmatches']['track'])
+
+            else
+              raise 'Unreachable'
+            end
+          end
+        end
+
+        class Result < Minerva::Fetcher::Result
+
+          # @return [String]
+          def title
+            item['name']
+          end
+
+          # @return [String]
+          def artist
+            item['artist']
+          end
+
+          # @return [String]
+          def image
+            item['image'].last.try(:[], '#text')
+          end
+
+          # @return [Hash]
+          def to_hash
+            attributes = [:title, :artist, :image]
+            Hash[attributes.map { |attribute| [attribute, send(attribute)] }]
+          end
+        end
+
+        # @param options [Hash]
+        def initialize(options = {})
+          options = {
+            api_key: "",
+            api_endpoint: "http://ws.audioscrobbler.com"
+          }.merge(options)
+
+          super(options)
+        end
+
+        # Searches Last.fm for the given title and optional artist
+        #
+        # @see http://www.lastfm.fr/api/show/track.getInfo
+        # @see http://www.lastfm.fr/api/show/track.search
+        # @param query [Query]
+        # @return [Array] A collection of Result objects
+        def fetch(query)
+          raise ArgumentError, "Please configure 'config.api_key' first" unless config.api_key.present?
+
+          case
+          when query.attributes[:title] && query.attributes[:artist]
+            params = {
+              method:  'track.getInfo',
+              track:   query.attributes[:title],
+              artist:  query.attributes[:artist],
+              format:  'json',
+              api_key: config.api_key,
+            }.merge(query.options)
+
+          when query.attributes[:title]
+            params = {
+              method:  'track.search',
+              track:   query.attributes[:title],
+              format:  'json',
+              api_key: config.api_key,
+            }.merge(query.options)
+
+          else
+            raise 'Unreachable'
+          end
+
+          response = Response.new(client.get('/2.0', params))
+          response.items.map { |item| Result.new(item) }
+        end
+
+        # @options [Hash]
+        # @return [Faraday::Connection]
+        def client(options = {})
+          options = {
+            url: config.api_endpoint
+          }.merge(options)
+
+          Faraday.new(options) do |faraday|
+            faraday.use FaradayMiddleware::ParseJson, content_type: /\bjson$/
+            faraday.use Faraday::Response::RaiseError
+            faraday.adapter Faraday.default_adapter
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/minerva-fetcher/strategies/lastfm_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+
+describe Minerva::Fetcher::Strategies::Lastfm do
+  let(:query)    { Minerva::Fetcher::Query.new(title: "Mesmerise") }
+  let(:strategy) { Minerva::Fetcher::Strategies::Lastfm.new }
+  let(:request)  { Minerva::Fetcher::Request.new(query, strategy) }
+
+  before { strategy.config.api_key = ENV['LASTFM_API_KEY'] }
+
+  describe Minerva::Fetcher::Strategies::Lastfm::Response do
+
+    describe :InstanceMethods do
+
+      describe :items, :skip do
+
+        context "with at least 1 result" do
+          it { expect().no_to be_empty }
+        end
+
+        context "with 0 result" do
+          it { expect().to be_empty }
+        end
+      end
+    end
+  end
+
+  describe Minerva::Fetcher::Strategies::Lastfm::Result do
+    let(:result) { request.results.first }
+
+    before { request.fetch }
+
+    describe :InstanceMethods do
+
+      describe :title do
+        it { expect(result.title).to be_a(String) }
+      end
+
+      describe :artist do
+        it { expect(result.artist).to be_a(String) }
+      end
+
+      describe :image do
+        it { expect(result.image).to be_a(String) }
+      end
+
+      describe :to_hash do
+        it { expect(result.to_hash).to be_a(Hash) }
+        it { expect(result.to_hash.keys).to eq([:title, :artist, :image]) }
+      end
+    end
+  end
+
+  describe :InstanceMethods do
+
+    describe :new do
+      pending
+    end
+
+    describe :fetch do
+
+      describe "without api_key" do
+        before { strategy.config.api_key = nil }
+        it { expect { strategy.fetch({}) }.to raise_error(ArgumentError) }
+      end
+
+      context "with a title and an artist attribute" do
+        let(:query) { Minerva::Fetcher::Query.new(title: "Mesmerise", artist: "Temples") }
+
+        it { expect(strategy.fetch(query)).to be_a(Array) }
+        it { expect(strategy.fetch(query).first).to be_a(Minerva::Fetcher::Strategies::Lastfm::Result) }
+      end
+
+      context "with a title attribute" do
+        let(:query) { Minerva::Fetcher::Query.new(title: "Mesmerise") }
+
+        it { expect(strategy.fetch(query)).to be_a(Array) }
+        it { expect(strategy.fetch(query).first).to be_a(Minerva::Fetcher::Strategies::Lastfm::Result) }
+      end
+    end
+
+    describe :client do
+      it { expect(strategy.client).to be_a(Faraday::Connection) }
+    end
+  end
+end
